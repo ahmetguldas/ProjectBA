6cf872a0921ffb53a5e7d229d059bdaaad7aee64
Ahmet GULDAS <ahmet.guldas@hotmail.com>
2021-09-22 15:12:48 +0300

    Ilk projenin UI katmani olusturuldu.

---
+++ EticaretBA.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EticaretBA.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
