30abfe30b858d0a2781036c3f09bc182e88ca045
ahmet güldaş <ahmet.guldas@hotmail.com>
2021-10-21 20:13:58 +0300

    New category Islemi

--- src/Web/Areas/Admin/Controllers/CategoriesController.cs
+++ src/Web/Areas/Admin/Controllers/CategoriesController.cs
@@ -1,10 +1,22 @@
+using System.Linq;
+using System.Security.Claims;
+using ApplicationCore.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Web.Areas.Admin.Models;
 
 namespace Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoriesController : Controller
     {
+        private readonly ApplicationDbContext db;
+
+        public CategoriesController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
         // GET
         public IActionResult Index()
         {
@@ -12,13 +24,31 @@ namespace Web.Areas.Admin.Controllers
             return View();
         }
 
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Yeni(NewCategoryViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                Category category = new Category()
+                
+                {
+                    CategoryName = vm.CategoryName
+                };
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
         public IActionResult New()
         {
             return View();
         }
 
-        public IActionResult Edit()
+        public IActionResult Edit(int id)
         {
+           
             return View();
         }
 
---
+++ src/Web/Areas/Admin/Models/NewCategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Areas.Admin.Models
+{
+    public class NewCategoryViewModel
+    {
+        [Required, MaxLength(100)]
+        public string CategoryName { get; set; }
+    }
+}
