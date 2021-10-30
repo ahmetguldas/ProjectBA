9835533fe7cbe38bab9a2d953ffdec3deaf40166
ahmet güldaş <ahmet.guldas@hotmail.com>
2021-10-30 04:18:19 +0300

    design and coding process of screens

--- src/Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -51,7 +51,7 @@ namespace Web.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Beni hatırla ?")]
             public bool RememberMe { get; set; }
         }
 
